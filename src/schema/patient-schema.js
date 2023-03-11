4642010558f64201380dba576e2f3857e1c2286b
zainasim <zainasim222@gmail.com>
2023-03-12 00:18:01 +0500

    Completed patient Signup backend

---
+++ src/schema/patient-schema.js
@@ -0,0 +1,16 @@
+import { date, object, string, ref } from 'yup';
+
+export const createPatientSchema = object({
+    body: object({
+        password: string()
+            .required('password is required')
+            .min(6, 'Password is too short - Should be min 6 characters')
+            .matches(/^[a-zA-z0-9_.-]*$/, 'Password can only contain Latin characters'),
+        confirmPassword: string().required('Confirm password is required').oneOf([ref('password')], 'Password did not match'),
+        email: string().email('Must be valid email').required('Email is rquired'),
+        firstName: string().required('First Name is required'),
+        lastName: string().required('Last Name is required'),
+        gender: string().required('Gender is required'),
+        dateOfBirth: date().required('Date of Birth is required'),
+    })
+});
