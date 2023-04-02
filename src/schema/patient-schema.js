f7544ae926bab3c8f0c24e99ad22725fb44ea7c1
zainasim <zainasim222@gmail.com>
2023-04-02 12:45:20 +0500

    changes

--- src/schema/patient-schema.js
+++ src/schema/patient-schema.js
@@ -5,7 +5,7 @@ export const createPatientSchema = object({
     body: object({
         password: string()
             .required('password is required')
-            .min(6, 'Password is too short - Should be min 6 characters')
+            .min(6, 'Password is too short - Should be min 6 characters'),
         confirmPassword: string().oneOf([ref('password')], 'Password did not match'),
         email: string().email('Must be valid email').required('Email is rquired'),
         firstName: string().required('First Name is required'),
